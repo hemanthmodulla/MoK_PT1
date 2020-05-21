5303467316dde609829a2a7e33ace03887185ede
heman <hemanthkumar.m13@gmail.com>
2020-05-21 00:31:13 -0400

    initial commit

---
+++ MoK_PT1_API/MoK_PT1_API/Models/Login.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoK_PT1_API.Models
+{
+
+    [Table("Login")]
+    public class Login
+    {
+        [Key]
+        public Guid id { get; set; }
+        public string userName { get; set; }
+        [PasswordPropertyText(true)]
+        public string password { get; set; }
+    }
+}
